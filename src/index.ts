a748faccdcd822d3c14708359861a25f3fafb14d
Yannick Ahlers <yannickahlers@gmail.com>
2021-09-28 23:59:45 +0200

    remove request from execute

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-export interface UseCase<Request> {
-  execute(request: Readonly<Request>): Promise<void> | void;
+export interface UseCase {
+  execute(): Promise<void> | void;
 }
 
-export type UseCaseFactory<T, R> = (port: T) => UseCase<R>;
+export type UseCaseFactory<T> = (port: T) => UseCase;
 
 export interface UseCaseController<E> {
-  handle(event: E): void;
+  handle(event: E): Promise<void> | void;
 }
